819640c194ab01fdf2ca8f35aff8c44f1d7de28f
unknown <gpupro@outlook.com>
2013-07-06 01:18:44 -0300

    Adding most of the AST node types.

---
+++ IDE/VisualSquirt/Util/Commands.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SqWrite.Util
+{
+	public static class Commands
+	{
+		public static readonly RoutedUICommand SaveAll = new RoutedUICommand("Save All", "SaveAll", typeof(MainWindow));
+		public static readonly RoutedUICommand Run = new RoutedUICommand("Run", "Run", typeof(MainWindow));
+		public static readonly RoutedUICommand StepThrough = new RoutedUICommand("Step Through", "StepThrough", typeof(MainWindow));
+		public static readonly RoutedUICommand StepIn = new RoutedUICommand("Step In", "StepIn", typeof(MainWindow));
+		public static readonly RoutedUICommand StepOut = new RoutedUICommand("Step Out", "StepOut", typeof(MainWindow));
+		public static readonly RoutedUICommand RunToCursor = new RoutedUICommand("Run To Cursor", "RunToCursor", typeof(MainWindow));
+	}
+}
